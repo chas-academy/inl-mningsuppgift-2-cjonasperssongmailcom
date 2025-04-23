963d2bebd3f722fba1f36074ac0e8f285c388518
cjonaspersson@gmail.com <cjonaspersson@gmail.com>
2025-04-23 16:57:52 +0200

    First_try

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
+/*
+    This program converts celsius to fahrenheit
+    and fahrenheit to celsius
+    Written by Jonas Persson 23.04.2025
+*/
 
 // This is where you add the two functions
+
+/*
+    Converts fahrenheit to celsius
+    returns a float
+*/
+float fahrenheit_to_celsius(float temp){
+    float celsius = ((temp - 32) * 5) / 9;
+    return celsius;
+}
+
+/*
+    Converts celsius to fahrenheit
+    returns a float
+*/
+float celsius_to_fahrenheit(float temp){
+    float fahrenheit = ((temp * 9) / 5) + 32;
+    return fahrenheit;
+}
 
 int main() {
     float temp_f = 100.0;
